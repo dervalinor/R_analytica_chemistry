11f588832e00f1d7bc76df66aef2a6b23808b5e7
dervalinor <dervalinor@protonmail.com>
2024-02-27 10:05:40 -0500

    introduccion a R

---
+++ 1_introduccion/primer_programa.R
@@ -0,0 +1,7 @@
+#instalar R-studio desde https://posit.co/download/rstudio-desktop/
+#apt install r-base
+#operaciones matematicas
+1+1
+#> es el simbolo de la terminal y es un prompt
+
+#como cargar datos txt en R
