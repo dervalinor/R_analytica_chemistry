d598125798ce568c960c35e0a8fa4bb5a9920438
dervalinor <dervalinor@protonmail.com>
2024-02-27 10:18:04 -0500

    Como cargar datos txt en R

--- 1_introduccion/primer_programa.R
+++ 1_introduccion/primer_programa.R
@@ -1,7 +1,17 @@
 #instalar R-studio desde https://posit.co/download/rstudio-desktop/
 #apt install r-base
 #operaciones matematicas
-1+1
+#1+1
+
+
+
 #> es el simbolo de la terminal y es un prompt
 
-#como cargar datos txt en R
+#como cargar datos txt en R- usar import databaset o por codigos
+datoscel <- read.table("~/R_analytica_chemistry/1_introduccion/datoscel.txt", header=TRUE, quote="\"")
+
+#ver datos, ejecutar en consola, el prompt es exclusivo de la consola
+#View(datoscel)
+
+#cargar datos en consola
+datoscel
