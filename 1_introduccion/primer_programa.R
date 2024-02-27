4dee7cad03cc35b7703bdda793adf79eb2aba454
dervalinor <dervalinor@protonmail.com>
2024-02-27 11:02:30 -0500

    crear graficos en R para analisis datos

--- 1_introduccion/primer_programa.R
+++ 1_introduccion/primer_programa.R
@@ -3,8 +3,6 @@
 #operaciones matematicas
 #1+1
 
-
-
 #> es el simbolo de la terminal y es un prompt
 
 #como cargar datos txt en R- usar import databaset o por codigos
@@ -13,5 +11,13 @@ datoscel <- read.table("~/R_analytica_chemistry/1_introduccion/datoscel.txt", he
 #ver datos, ejecutar en consola, el prompt es exclusivo de la consola
 #View(datoscel)
 
-#cargar datos en consola
+#cargar datos en consola o cargar variable
 datoscel
+
+#separar la base de datos en columnas y considera vectores no como un matriz
+attach(datoscel)
+
+#visitar Rpubs para encontrar tutoriales de manejo de R
+
+#IMPORTANTE: es importante antes de hacer un experimentos que se van ha 
+#hacer con estos datos  
---
+++ 2_tratar_datos/analisis_datos.R
@@ -0,0 +1,39 @@
+#DISEÑO ALEATORIZADO
+#Caso: Vamos a considerar un experimento en el que 30 ratones tuvieron una condición de leucemia inducida y 
+#fueron aleatorizados en tres grupos de 10 animales. Cada grupo fue tratado con uma droga quimioterápica y 
+#cada animal fue observado en cuatro  ocasiones. Las siguientes variables fueron observadas:
+
+#Celulas: número de colonias de células cancerosas
+
+#wbc: número de células blancas
+
+#rbc: número de células rojas
+
+#OBJETIVO: Afectan los tratamientos a la enfermedad
+
+#Diseño de experimentos: debe hacer una variable que uno controla y como afecta a otra variable
+
+#ESTUDIARentender que es un variables en estadisticas y como se analizan y que modelos se utilizan
+
+#como cargar datos txt en R- usar import databaset o por codigos
+datoscel <- read.table("~/R_analytica_chemistry/1_introduccion/datoscel.txt", header=TRUE, quote="\"")
+#ver datos en otra ventana: View(datoscel)
+
+#separar datos en vectores y poder trabajar en ellas
+attach(datoscel) #importante colocar esto para trabajar con los datos !!!!!!
+
+#tratamientos es un variable categorica o cualitativa
+#variable de celulas es la variable de respuesta debe ser cuantitativa
+#siempre un analisis de forma descriptiva
+
+#EStudiar histogramas, graficos de barra
+
+#histograma de la variable de celulas para analizar un sola variable
+#hist(celulas) #no sirve para el analisis
+#graficos de tortas
+#pie(celulas)
+
+#pero tambien existen formas de analizar dos variables para saber si los tratamientos sirven
+#graficos de boxplot util para esta caso
+boxplot(celulas ~ trat) #celulas vs trat, tratamiento 2 no recomendable por que tiene mas celulas cancerigenas
+#pero el tratamiento 1 es mas recomendable menos celulas cancerigenas
