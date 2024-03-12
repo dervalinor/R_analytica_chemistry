524a3eb452d47e8f7c449072fb0a11d142418265
dervalinor <dervalinor@protonmail.com>
2024-03-12 10:06:18 -0500

    Como crear un ANOVA

---
+++ 5_clase_3/clase3.R
@@ -0,0 +1,77 @@
+
+library(agricolae) 
+tratamientos <- c("T1", "T2", "T3", "T4") 
+
+
+tratamientos
+
+
+replicas = c(5, 5, 5, 5)
+replicas
+
+
+?design.crd 
+design.crd(tratamientos, replicas) 
+
+
+
+design.crd(tratamientos, replicas, seed = 20)
+
+mi_tabla <- design.crd(tratamientos, replicas, seed = 20)
+mi_tabla
+
+ 
+tabla_diseno <- mi_tabla$book
+
+
+
+set.seed(1010)
+
+
+respuestas <- rnorm(20, mean = 5, sd = 2) 
+respuestas
+
+
+
+plot(density(respuestas))
+tabla_diseno$respuestas = respuestas 
+tabla_diseno
+
+attach(tabla_diseno)
+
+#analisis de un variable cualitativa vs variable cuantitativa
+boxplot(respuestas ~ tratamientos, data = tabla_diseno, 
+        col = c("#2F8FE7", "#B849E0", "#49E0C6", "#70E049"), 
+        xlab = "Tratamientos", 
+        ylab = "Respuestas",
+        main = "Experimento simulado")
+
+#pero necesitamos que nuestras conclusiones sean validas . usa la estadistica inferencial
+#los tratamientos afectan la variable respuesta
+#Respueta observada = media general + efecto tratamiento + Error
+
+#hipotesis en la estadistica, existe varias tecnicas como p-valor (permite dicidir H oscila 0 < p < 1) y existe un nivel de significancia
+#que indica la probabilidad de cometer un error, el nivel de significancia sea pequeño lo mas pequeño posible, es decir hacer
+# el mayor numero de replicas
+
+# Realizar la prueba ANOVA
+
+names(tabla_diseno) = c("unidades experimentales", "Replica", "Tratamientos", "Respuestas") #cambiar nombre de las columna de las 
+#tablas
+
+attach(tabla_diseno) #no olvidar esto !!!!!
+
+#ANOVA de un via, ya que hay un solo factor de juego es decir tratamientos
+modelo <- aov(Respuestas ~ Tratamientos, data = tabla_diseno)
+resumen <- summary(modelo)
+
+# Obtener el valor p
+valor_p <- resumen[[1]][["Pr(>F)"]][1]
+
+# Evaluar la hipótesis nula
+#se rechaza H cuando el nivel p < niveles de significancia en caso contrario no rechazo H
+if (valor_p < 0.05) {
+  cat("Se rechaza la hipótesis nula. Al menos una media es diferente.\n")
+} else {
+  cat("No se puede rechazar la hipótesis nula. Las medias son iguales.\n")
+}
