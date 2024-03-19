b4245b41f8a95d1c2ac9084f600d1309ce2bb4d2
dervalinor <dervalinor@protonmail.com>
2024-03-19 10:50:51 -0500

    Experimento 1

--- 6_clase_4/experimento.R
+++ 6_clase_4/experimento.R
@@ -33,6 +33,16 @@ datos_dietas
 
 attach(datos_dietas)
 
+#hacer analisis exploratorio
+# Calculando estadísticas descriptivas
+summary(datos_dietas)
+
+# Visualización de los datos
+boxplot(respuestas ~ tratamientos, data = datos_dietas,
+        xlab = "Tipo de Dieta",
+        ylab = "Tiempo de Coagulación",
+        main = "Boxplot de Tiempo de Coagulación por Tipo de Dieta") # no es suficiente para ver diferencias
+ 
 #chatGPT:
 
 # Realizar la prueba ANOVA
@@ -45,6 +55,66 @@ p_value <- summary(anova_result)[[1]]$`Pr(>F)`[1]
 alpha <- 0.05
 
 # Condición para determinar la conclusión del experimento, Qe significado tiene matematico y fisico de esto 
+if (p_value < alpha) {
+  cat("Dado el valor p de", p_value, "se concluye que hay diferencias significativas entre al menos dos grupos de dieta. Por lo tanto, el tipo de dieta tiene un efecto significativo en el tiempo de coagulación de la sangre.")
+} else {
+  cat("Dado el valor p de", p_value, "no se encontraron diferencias significativas entre los grupos de dieta. Por lo tanto, no se puede concluir que el tipo de dieta tenga un efecto significativo en el tiempo de coagulación de la sangre.")
+}
+
+#Evaluar los supuestos: normalidad
+
+residuales = residuals(anova_result)
+residuales
+
+#graficos
+#hacer un dos graficos
+par(mfrow = c(1, 2))
+plot(density(residuales))
+plot(anova_result, which = 2)
+
+shapiro.test(residuales)
+
+#quitar para evitar, quitar graficos
+dev.off()
+
+#evaluar homocedasticidad 
+
+#modo grafica
+plot(anova_result, which = 1)
+
+# Cargar el paquete necesario
+library(car)
+
+# Realizar la prueba de bartlett para evaluar la homocedasticidad
+# Realizar la prueba de Bartlett para evaluar la homocedasticidad
+bartlett_test <- bartlett.test(respuestas ~ tratamientos, data = datos_dietas)
+bartlett_test
+
+# Realizar la prueba de Bartlett para evaluar la homocedasticidad
+bartlett_test <- bartlett.test(respuestas ~ tratamientos, data = datos_dietas)
+
+# Obtener el valor p del test de Bartlett
+p_value_bartlett <- bartlett_test$p.value
+
+# Nivel de significancia
+alpha <- 0.05
+
+# Condición para determinar la hipótesis nula de Bartlett
+if (p_value_bartlett < alpha) {
+  cat("La hipótesis nula de Bartlett (H0) es que las varianzas de las muestras provienen de poblaciones con la misma varianza.\n")
+} else {
+  cat("La hipótesis nula de Bartlett (H0) no puede ser rechazada. Las varianzas de las muestras pueden provenir de poblaciones con la misma varianza.\n")
+}
+
+#test  de independencia
+plot(1:24, residuales, pch = 19) #no deben existir patrones
+
+#interpretacion ANNOVA
+summary(anova_result)
+
+#esto no es blanco y negro, no es asi, ver que consecuencias de esto en un experimento, ver si puede replicar, siempre planeas
+#balaceado
+
 if (p_value < alpha) {
   cat("Dado el valor p de", p_value, "se concluye que hay diferencias significativas entre al menos dos grupos de dieta. Por lo tanto, el tipo de dieta tiene un efecto significativo en el tiempo de coagulación de la sangre.")
 } else {
