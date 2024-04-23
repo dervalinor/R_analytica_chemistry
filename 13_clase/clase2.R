59928a8e846151633db5396b3fe2ed2957c6745d
dervalinor <dervalinor@protonmail.com>
2024-04-23 16:12:16 +0000

    Analisis de bloques

---
+++ 13_clase/clase2.R
@@ -0,0 +1,6 @@
+#Suponga que quiere determinarse si cuatro puntas diferentes producen
+#o no lecturas diferentes en una máquina para probar la dureza. Un
+#experimento como este podrı́a ser parte de un estudio de la aptitud
+#en la calibración de los instrumentos.
+
+
