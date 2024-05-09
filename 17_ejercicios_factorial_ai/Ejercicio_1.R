0fae9847456d420f87060146783108db816b26ca
Miguel Ángel Riascos <dervalinor@protonmail.com>
2024-05-09 00:16:20 +0000

    Si se logro hacer un diseño factorial y solo falta las comparaciones multiples

--- 17_ejercicios_factorial_ai/Ejercicio_1.R
+++ 17_ejercicios_factorial_ai/Ejercicio_1.R
@@ -14,15 +14,15 @@
 #\hline
 #Adiciones de etanol & aire/combustible & emisiones CO \\
 #\hline
-#0,1 & 14 & 66;62 \\
-#0,1 & 15 & 72;67 \\
-#0,1 & 16 & 68;66 \\
-#0,2 & 14 & 78;81 \\
-#0,2 & 15 & 80;81 \\
-#0,2 & 16 & 66;69 \\
-#0,3 & 14 & 90;94 \\
-#0,3 & 15 & 75;78 \\
-#0,3 & 16 & 60;58 \\
+#0,1 & 14 & 66;62 \\1
+#0,1 & 15 & 72;67 \\2
+#0,1 & 16 & 68;66 \\3
+#0,2 & 14 & 78;81 \\4
+#0,2 & 15 & 80;81 \\5
+#0,2 & 16 & 66;69 \\6
+#0,3 & 14 & 90;94 \\7
+#0,3 & 15 & 75;78 \\8
+#0,3 & 16 & 60;58 \\9
 #\hline
 #\end{tabular}
 
@@ -32,15 +32,36 @@
 
 #Es importante convertir a factor las adiciones de etanol y aire/combustible en
 #factores
+library(agricolae)
 
-addicion_etanol = rep(rep(c("0.1", "0.2", "0.3"), each = 3), 2)
+addicion_etanol = rep(c("0.1", "0.2", "0.3"), each = 6)
 addicion_etanol
+length(addicion_etanol)
 
 aire_combustible = rep(rep(c("14", "15", "16"), 3), each = 2)
 aire_combustible
+length(aire_combustible)
 
 #solucionar como agregar datos
-emisiones_co = c(c(66,62), c(72,67), c(68, 66), c(78, 81), c(66, 69), c(90, 94), c(75, 78), c(60, 58))
+emisiones_co = c(66,62,72,67,68,66,78,81,80,81,66,69,90,94,75,78, 60,58) 
+#IMPORTANTE VER QUE LOS DATOS SE INTRODUCCIERON BIEN
+#Si si se puedo todo salio bien
 emisiones_co
+length(emisiones_co)
 
 datos_emisiones = data.frame(emisiones_co, addicion_etanol, aire_combustible)
+datos_emisiones
+attach(datos_emisiones)
+
+#Analisis de ANOVA
+
+modelo_emisiones = aov(emisiones_co ~ addicion_etanol * aire_combustible, 
+                       data = datos_emisiones)
+summary(modelo_emisiones) #el mismo resultado que el profesor en el pdf
+
+dev.off()
+
+#grafico de interaccion
+interaction.plot(addicion_etanol, aire_combustible, emisiones_co,
+                 xlab = "Adiciones Etanol", ylab = "Aire/Combustible",
+                 trace.label = "Emisiones de CO")
